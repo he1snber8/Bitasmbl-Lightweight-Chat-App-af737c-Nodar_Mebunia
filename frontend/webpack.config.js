dc93f34fea21bc1c5582580091a289b17b9fe619
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-22 13:16:40 +0400

    Add webpack.config.js

---
+++ frontend/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: { path: path.resolve(__dirname, 'dist'), filename: 'bundle.js' },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader', options: { presets: ['@babel/preset-react'] } }
+      }
+    ]
+  },
+  resolve: { extensions: ['.js', '.jsx'] },
+  devServer: { static: './public', port: 3000 }
+};
